ece0e0e837617ff68367a52846ece131cae2aa07
LEVIOS\54725 <lorewalkeryu@foxmail.com>
2022-08-07 22:32:21 +0800

    T19 render a colored img, and addd index buffer

--- Project1/Source/Graphics.cpp
+++ Project1/Source/Graphics.cpp
@@ -1,8 +1,8 @@
 #include "../Header/Graphics.h"
 #include "../dxerr/dxerr.h"
-#include <sstream>
 #include "../Header/Window.h"
 #include <d3dcompiler.h>
+#include <sstream>
 
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "D3DCompiler.lib")
@@ -34,18 +34,32 @@ void Graphics::DrawTriangle()
 	// Vertex format
 	struct Vertex
 	{
-		float x;
-		float y;
+		struct
+		{
+			float x;
+			float y;
+		}pos;
+		struct
+		{
+			unsigned char r;
+			unsigned char g;
+			unsigned char b;
+			unsigned char a;
+		}color;
 	};
 
 
 	const Vertex vertices[] =
 	{
-		{ 0.f, 0.5f },
-		{ 0.5f, -0.5f },
-		{ -0.5f, -0.5f }
+		{ 0.f, 0.5f, 255, 255, 0, 1},
+		{ 0.5f, -0.5f, 0, 255, 0, 1},
+		{ -0.5f, -0.5f, 0, 0, 255, 1},
+		{ -0.3f, 0.3f, 0, 255, 0, 1},
+		{ 0.3f, 0.3f, 0, 0, 255, 1},
+		{ 0.f, -0.8f, 255, 0, 0, 1},
 	};
 
+	// create vertex buffer
 	wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
 	D3D11_BUFFER_DESC bd = {};
 	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
@@ -60,12 +74,38 @@ void Graphics::DrawTriangle()
 
 	GFX_THROW_INFO(pDevice->CreateBuffer(&bd, &sd, &pVertexBuffer));
 
-
+	// bind vertex buffer to pipeline
 	const UINT stride = sizeof(Vertex);
 	const UINT offset = 0u;
 
 	pContext->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
 
+
+
+	// Create index buffer
+	const unsigned short indices[] =
+	{
+		0, 1, 2,
+		0, 2, 3,
+		0, 4, 1,
+		2, 1, 5
+	};
+	wrl::ComPtr<ID3D11Buffer> pIndexBuffer;
+	D3D11_BUFFER_DESC ibd = {};
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.Usage = D3D11_USAGE_DEFAULT;
+	ibd.CPUAccessFlags = 0u;
+	ibd.MiscFlags = 0u;
+	ibd.ByteWidth = sizeof(indices);
+	ibd.StructureByteStride = sizeof(unsigned short);
+
+	D3D11_SUBRESOURCE_DATA isd = {};
+	isd.pSysMem = indices;
+
+	GFX_THROW_INFO(pDevice->CreateBuffer(&ibd, &isd, &pIndexBuffer));
+
+	// bind index buffer to pipeline
+	pContext->IASetIndexBuffer(pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u);
 
 
 	wrl::ComPtr<ID3DBlob> pBlob;
@@ -93,7 +133,8 @@ void Graphics::DrawTriangle()
 	wrl::ComPtr<ID3D11InputLayout> pInputLayout;
 	const D3D11_INPUT_ELEMENT_DESC ied[] =
 	{
-		{"Position", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA,0 }
+		{"Position", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA,0 }, 
+		{"Color", 0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, 8u, D3D11_INPUT_PER_VERTEX_DATA,0 }
 	};
 	GFX_THROW_INFO(pDevice->CreateInputLayout(
 		ied, (UINT)std::size(ied),
@@ -104,7 +145,7 @@ void Graphics::DrawTriangle()
 	// bind input layout
 	pContext->IASetInputLayout(pInputLayout.Get());
 	
-	//bind render target
+	// bind render target
 	pContext->OMSetRenderTargets(1u, pTarget.GetAddressOf(), nullptr);
 
 	// set primitive topology to triangle list
@@ -121,7 +162,7 @@ void Graphics::DrawTriangle()
 	pContext->RSSetViewports(1u, &vp);
 
 
-	GFX_THROW_INFO_ONLY( pContext->Draw((UINT)std::size(vertices), 0u));
+	GFX_THROW_INFO_ONLY( pContext->DrawIndexed((UINT)std::size(indices), 0u, 0u));
 }
 
 Graphics::Graphics(HWND hWnd)
