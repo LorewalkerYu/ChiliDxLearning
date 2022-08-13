4716aa970e6f254f59583b086899e3fe5e74afc1
LEVIOS\54725 <lorewalkeryu@foxmail.com>
2022-08-14 03:57:14 +0800

    fix specular : 1. change object  postion from world position to camera position 2,  change camera  postion from world position to camera position
    add attenuation to specular(PhongPS.hlsl)

--- Project1/Source/Pipeline/Lights/PointLight.cpp
+++ Project1/Source/Pipeline/Lights/PointLight.cpp
@@ -57,8 +57,18 @@ void PointLight::Draw(Graphics& gfx) const noexcept
 	mesh.Draw(gfx);
 }
 
-void PointLight::Bind(Graphics& gfx) const noexcept
+//void PointLight::Bind(Graphics& gfx) const noexcept
+//{
+//	cbuf.Update(gfx, cbData);
+//	cbuf.Bind(gfx);
+//}
+
+
+void PointLight::Bind(Graphics& gfx, DirectX::FXMMATRIX view) const noexcept
 {
-	cbuf.Update(gfx, cbData);
+	auto dataCopy = cbData;
+	const auto pos = DirectX::XMLoadFloat3(&cbData.pos);
+	DirectX::XMStoreFloat3(&dataCopy.pos, DirectX::XMVector3Transform(pos, view));
+	cbuf.Update(gfx, dataCopy);
 	cbuf.Bind(gfx);
 }
