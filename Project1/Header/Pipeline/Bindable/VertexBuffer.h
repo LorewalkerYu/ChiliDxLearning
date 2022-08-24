68e4e36df7927b17cdc0deb7908b5978cb890293
LEVIOS\54725 <lorewalkeryu@foxmail.com>
2022-08-25 01:52:12 +0800

    bindable codex prototype

--- Project1/Header/Pipeline/Bindable/VertexBuffer.h
+++ Project1/Header/Pipeline/Bindable/VertexBuffer.h
@@ -6,28 +6,7 @@ namespace Bind
 	class VertexBuffer : public Bindable
 	{
 	public:
-		template<class V>
-		VertexBuffer(Graphics& gfx, const std::vector<V>& vertices)
-			:
-			stride(sizeof(V))
-		{
-			INFOMAN(gfx);
 
-			// create vertex buffer
-
-			D3D11_BUFFER_DESC bd = {};
-			bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-			bd.Usage = D3D11_USAGE_DEFAULT;
-			bd.CPUAccessFlags = 0u;
-			bd.MiscFlags = 0u;
-			bd.ByteWidth = vertices.size() * sizeof(V);
-			bd.StructureByteStride = sizeof(V);
-
-			D3D11_SUBRESOURCE_DATA sd = {};
-			sd.pSysMem = vertices.data();
-
-			GFX_THROW_INFO(GetDevice(gfx)->CreateBuffer(&bd, &sd, &pVertexBuffer));
-		}
 		VertexBuffer(Graphics& gfx, const Dvtx::VertexBuffer& vbuf)
 			:
 			stride((UINT)vbuf.GetLayout().Size())
